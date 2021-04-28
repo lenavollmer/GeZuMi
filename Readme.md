6056decf7b05bd24ef5ba57f20e108ccb31ea64e
Florian Reitz <flo@reitz.dev>
2021-04-28 16:02:10 +0200

    'init'

---
+++ Readme.md
@@ -0,0 +1,4 @@
+
+
+Create local.properties file with content:
+Windows: `sdk.dir=C\:\\Users\\<USER>\\AppData\\Local\\Android\\Sdk`
---
+++ app/src/main/java/de/htw/gezumi/MainFragment.kt
@@ -0,0 +1,34 @@
+package de.htw.gezumi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_host).setOnClickListener {
+            findNavController().navigate(R.id.action_MainFragment_to_Host)
+        }
+        view.findViewById<Button>(R.id.button_join).setOnClickListener {
+            findNavController().navigate(R.id.action_MainFragment_to_Join)
+        }
+    }
+}
