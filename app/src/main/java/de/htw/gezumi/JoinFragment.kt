702a3d5f8b574c5563f25a35c365d1b43d092948
Florian Reitz <flo@reitz.dev>
2021-04-28 19:32:55 +0200

    started work on GATT Service

--- app/src/main/java/de/htw/gezumi/JoinFragment.kt
+++ app/src/main/java/de/htw/gezumi/JoinFragment.kt
@@ -1,12 +1,19 @@
 package de.htw.gezumi
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattService
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import java.util.*
+
+
+val MY_SERVICE: UUID = UUID.fromString("e0ec8d9c-5e4d-470a-b87f-64f433685301")
+val MY_CHARACTERISTIC: UUID = UUID.fromString("e0ec8d9c-5e4d-470a-b87f-64f433685302")
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -19,6 +26,27 @@ class JoinFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_join, container, false)
+    }
+
+    /**
+     * Return a configured [BluetoothGattService] instance for the
+     * Custom Service.
+     */
+    fun createCustomBleService(): BluetoothGattService {
+        val service = BluetoothGattService(
+            MY_SERVICE,
+            BluetoothGattService.SERVICE_TYPE_PRIMARY
+        )
+
+        // Current Configuration characteristic
+        val characteristic = BluetoothGattCharacteristic(
+            MY_CHARACTERISTIC,
+            BluetoothGattCharacteristic.PROPERTY_READ or BluetoothGattCharacteristic.PROPERTY_WRITE,
+            BluetoothGattCharacteristic.PERMISSION_READ or BluetoothGattCharacteristic.PERMISSION_WRITE
+        )
+        val serviceAdded: Boolean = service.addCharacteristic(characteristic)
+        print(serviceAdded)
+        return service
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
