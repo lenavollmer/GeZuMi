ee227e7e178e58c3259e634d00851e6607ba2c36
Samuel Günther <samuell.guenther@gmail.com>
2021-05-10 20:10:33 +0200

    use raw bytes for bluetooth address

--- app/src/main/java/de/htw/gezumi/model/DeviceData.kt
+++ app/src/main/java/de/htw/gezumi/model/DeviceData.kt
@@ -9,9 +9,8 @@ class DeviceData(val deviceAddress: String, val value: Float): Serializable {
         const val separator = 0x0A.toByte()
 
         fun fromBytes(bytes: ByteArray): DeviceData {
-            val separatorIndex = indexOf(bytes, separator)
-            val deviceAddress = bytes.slice(0 until separatorIndex).toByteArray().toString(Charsets.UTF_8)
-            val value = ByteBuffer.wrap(bytes.slice(separatorIndex + 2 until bytes.size).toByteArray()).float
+            val deviceAddress = bytes.slice(0 until 6).toByteArray().toString(Charsets.UTF_8)
+            val value = ByteBuffer.wrap(bytes.slice(6 until bytes.size).toByteArray()).float
             return DeviceData(deviceAddress, value)
         }
 
@@ -35,6 +34,26 @@ class DeviceData(val deviceAddress: String, val value: Float): Serializable {
         bos.close()
         return bytes*/
         // newline \n = 0x0A
-        return deviceAddress.toByteArray(Charsets.UTF_8) + separator + ByteBuffer.allocate(4).putFloat(value).array()
+        return stringToByteAddress(deviceAddress, 6) + ByteBuffer.allocate(4).putFloat(value).array()
+    }
+
+    private fun stringToByteAddress(stringAddress: String, numBytes: Int): ByteArray {
+        var bytes = mutableListOf<Byte>()
+        var start = 0
+        for (i in stringAddress.indices) {
+            if (stringAddress[i] == ':' || i == stringAddress.length - 1) {
+                bytes.add(stringAddress.slice(start until i).toByte())
+                start = i + 1
+            }
+        }
+        return bytes.toByteArray();
+    }
+
+    private fun byteAddressToString(bytes: ByteArray, numBytes: Int): String {
+        /*var string = mutableListOf<Char>()
+        for (byte in bytes) {
+            string.add(by)
+        }*/
+        return bytes.toString(Charsets.UTF_8)
     }
 }
