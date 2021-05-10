e7a2bf651f231b5930cd3fc7f31c057d8a5aba69
Samuel Günther <samuell.guenther@gmail.com>
2021-05-10 20:25:04 +0200

    bytes to hex not working

--- app/src/main/java/de/htw/gezumi/model/DeviceData.kt
+++ app/src/main/java/de/htw/gezumi/model/DeviceData.kt
@@ -1,7 +1,8 @@
 package de.htw.gezumi.model
 
-import java.io.*
+import java.io.Serializable
 import java.nio.ByteBuffer
+import kotlin.experimental.and
 
 class DeviceData(val deviceAddress: String, val value: Float): Serializable {
 
@@ -9,7 +10,7 @@ class DeviceData(val deviceAddress: String, val value: Float): Serializable {
         const val separator = 0x0A.toByte()
 
         fun fromBytes(bytes: ByteArray): DeviceData {
-            val deviceAddress = bytes.slice(0 until 6).toByteArray().toString(Charsets.UTF_8)
+            val deviceAddress = bytesToHex(bytes.slice(0 until 6).toByteArray())
             val value = ByteBuffer.wrap(bytes.slice(6 until bytes.size).toByteArray()).float
             return DeviceData(deviceAddress, value)
         }
@@ -20,6 +21,18 @@ class DeviceData(val deviceAddress: String, val value: Float): Serializable {
                     return i;
             }
             return -1;
+        }
+
+        private val HEX_ARRAY = "0123456789ABCDEF".toCharArray()
+
+        private fun bytesToHex(bytes: ByteArray): String {
+            val hexChars = CharArray(bytes.size * 2)
+            for (j in bytes.indices) {
+                val v: Int = bytes[j] and 0xFF
+                hexChars[j * 2] = HEX_ARRAY.get(v ushr 4)
+                hexChars[j * 2 + 1] = HEX_ARRAY.get(v and 0x0F)
+            }
+            return String(hexChars)
         }
 
     }
@@ -34,7 +47,8 @@ class DeviceData(val deviceAddress: String, val value: Float): Serializable {
         bos.close()
         return bytes*/
         // newline \n = 0x0A
-        return stringToByteAddress(deviceAddress, 6) + ByteBuffer.allocate(4).putFloat(value).array()
+        val hexString = deviceAddress.replace(":", "")
+        return hexStringToByteArray(hexString) + ByteBuffer.allocate(4).putFloat(value).array()
     }
 
     private fun stringToByteAddress(stringAddress: String, numBytes: Int): ByteArray {
@@ -48,12 +62,24 @@ class DeviceData(val deviceAddress: String, val value: Float): Serializable {
         }
         return bytes.toByteArray();
     }
-
-    private fun byteAddressToString(bytes: ByteArray, numBytes: Int): String {
-        /*var string = mutableListOf<Char>()
+/*
+    private fun byteAddressToString(bytes: ByteArray): String {
+        var string = mutableListOf<Char>()
         for (byte in bytes) {
             string.add(by)
-        }*/
+        }
         return bytes.toString(Charsets.UTF_8)
+    }
+*/
+    private fun hexStringToByteArray(s: String): ByteArray {
+        val len = s.length
+        val data = ByteArray(len / 2)
+        var i = 0
+        while (i < len) {
+            data[i / 2] = ((Character.digit(s[i], 16) shl 4)
+                    + Character.digit(s[i + 1], 16)).toByte()
+            i += 2
+        }
+        return data
     }
 }
